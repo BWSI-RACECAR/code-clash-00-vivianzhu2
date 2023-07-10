6afbdd96d08ab7ebeaa9da70533d430d0cd7a655
Vivian Zhu <vivian.yijia.zhu@gmail.com>
2023-07-10 16:41:31 -0700

    Code Cash 0

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+class Solution:
+    def helloworld(self,string):
+        return(string)
+def main():
+    tc1 = Solution()
+    string1= input()
+    ans = tc1.helloworld(string1)
+    print(ans)
+if __name__ == "__main__":  
+    main()
